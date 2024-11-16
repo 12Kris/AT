69687385b1dc6ce681801825958ef380de2d29da
12Kris <116078485+12Kris@users.noreply.github.com>
2024-11-17 00:09:36 +0200

    task 10

--- labs/src/test/java/task_10/Task_10.java
+++ labs/src/test/java/task_10/Task_10.java
@@ -1,5 +1,17 @@
 package task_10;
 
+/*
+1. Set up ChromeDriver using the driver file and properties.
+2. Set up ChromeDriver using DriverManager.
+3. Navigate to the page assigned to your variant.
+4. Select any three different elements.
+5. Interact with all the selected elements.
+6. Verify the visibility of each element using assertions.
+7. Wrap all these steps into a TestNG test case.
+
+V10. https://demoqa.com (checkbox)
+*/
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,34 +22,36 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Task_10 {
-    WebDriver webDriver;
+    WebDriver driver;
 
     @BeforeTest
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-        webDriver = new ChromeDriver();
+        driver = new ChromeDriver();
     }
 
     @Test
-    public void task10Test() {
-        webDriver.get("https://demoqa.com/checkbox");
+    public void task10Test() throws InterruptedException {
+        driver.get("https://demoqa.com/checkbox");
+        Thread.sleep(2000);
 
-        WebElement expandAllToggleIcon = webDriver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/span/button"));
-        WebElement checkboxItemDocumentsToggleIcon = webDriver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[2]/span/button"));
-        WebElement documentsItemVeu = webDriver.findElement(By.id("tree-node-veu"));
+        WebElement expandAllIcon = driver.findElement(By.xpath("//button[@title='Expand all']"));
+        WebElement documentsItemVeu = driver.findElement(By.id("tree-node-veu"));
+        WebElement collapseAllIcon = driver.findElement(By.xpath("//button[@title='Collapse all']"));
 
-        expandAllToggleIcon.click();
-        checkboxItemDocumentsToggleIcon.click();
+
+        expandAllIcon.click();
         documentsItemVeu.click();
+        collapseAllIcon.click();
 
-        Assert.assertTrue(expandAllToggleIcon.isDisplayed(), "Expand All button is not visible.");
-        Assert.assertTrue(checkboxItemDocumentsToggleIcon.isDisplayed(), "Documents is not visible.");
+        Assert.assertTrue(expandAllIcon.isDisplayed(), "Expand All button is not visible.");
         Assert.assertTrue(documentsItemVeu.isDisplayed(), "Item Veu is not visible");
+        Assert.assertTrue(collapseAllIcon.isDisplayed(), "Collapse ALL button is not visible.");
     }
 
     @AfterTest
     public void closeBrowser() {
-        webDriver.close();
-        webDriver.quit();
+        driver.close();
+        driver.quit();
     }
 }
